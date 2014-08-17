03879ca7a9c4cb2d856921f957754a70b0e1c7cf
Lucas Lima <lapl@cin.ufpe.br>
2014-08-17 00:58:12 -0300

    codigo tinha sumido Oo

--- mainapp/views.py
+++ mainapp/views.py
@@ -177,6 +177,27 @@ def getEvent(request):
     retEvent = event.getDetailedEvent(fb_user)
     return HttpResponse(json.dumps({"event" : retEvent}), content_type="application/json")
 
+def enterEvent(request):
+    data = json.loads(request.read())
+    access_token = data['access_token']
+    profile, fb_user = connect(request, access_token)
+    eventId = data['id']
+    event = Event.objects.get(id=eventId)
+    event.persons.add(profile)
+    event.save()
+    return HttpResponse(json.dumps(event.getEvent(fb_user)), content_type="application/json")
+
+def leaveEvent(request):
+    data = json.loads(request.read())
+    access_token = data['access_token']
+    profile, fb_user = connect(request, access_token)
+    eventId = data['id']
+    event = Event.objects.get(id=eventId)
+    event.persons.remove(profile)
+    event.save()
+    return HttpResponse(json.dumps(event.getEvent(fb_user)), content_type="application/json")
+
+
 def arrive(request):
     data = json.loads(request.read())
     access_token = data['access_token']
